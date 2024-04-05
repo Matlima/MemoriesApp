5cd9676f278ec69cbfba2873eed3205835137b23
Matheus <limarios18@outlook.com>
2024-04-05 00:14:24 -0300

    criando barra de navegação e footer

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <h2>
+            <Link to="/">Memories</Link>
+        </h2>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/add-memory">Adicionar Memória</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
