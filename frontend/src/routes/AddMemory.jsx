1f3dfcc0c104f5f3205ec6c790a8bda07d63300e
Matheus <limarios18@outlook.com>
2024-04-05 00:31:26 -0300

    Criando formulário de memoria e css

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Axios } from "axios";
+
+import { useState } from "react";
+
+import "./AddMemory.css";
 
 const AddMemory = () => {
   return (
-    <div>AddMemory</div>
+    <div className="add-memory-page">
+        <h2>Crie uma nova memória</h2>
+        <form action="">
+            <label>
+                <p>Título:</p>
+                <input type="text" name="title" placeholder="Defina um titulo" />
+            </label>
+            <label>
+                <p>Descrição:</p>
+                <textarea name="description" placeholder="Explique o que aconteceu..."> </textarea>
+            </label>
+            <label>
+                <p>Foto:</p>
+                <input type="file" name="image" />
+            </label>
+            <input type="submit" value="Enviar" className="btn" />
+
+        </form>
+    </div>
   )
 }
 
